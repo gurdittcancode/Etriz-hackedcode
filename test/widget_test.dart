b99d28735643ab7deeb649554ba50bfa4a4671e6
Oitanny <mannyk778@gmail.com>
2024-04-26 13:14:51 +0530

    codebase setup itr-1

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,8 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:groupshot/main.dart';
 
-import 'package:hackedcode/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
